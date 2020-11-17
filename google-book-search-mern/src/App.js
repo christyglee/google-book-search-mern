45c145ce795eed686d2a28646c8bf15235ae8c0e
reng <reng412@gmail.com>
2020-11-17 13:42:55 -0800

    Add header component

--- google-book-search-mern/src/App.js
+++ google-book-search-mern/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
     return (
         <div>
-            <p>Hello!</p>
+            <Header></Header>
         </div>
     );
 }
