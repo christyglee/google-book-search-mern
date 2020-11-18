59d191e41daa1ffcfac6d44364407243cfeb7018
reng <reng412@gmail.com>
2020-11-18 10:45:04 -0800

    Update server and package.json files to run in different environments

---
+++ google-book-search-mern/src/components/Search/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+    return (
+        <div className="container">
+            <h1>Saved Books</h1>
+
+        </div>
+    );
+}
+
+export default Search;
