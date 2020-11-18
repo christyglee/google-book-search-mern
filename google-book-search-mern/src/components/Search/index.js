e4ee5d340011ec9f349af27db1ff42e81b75abbd
reng <reng412@gmail.com>
2020-11-18 10:56:10 -0800

    Add search component

--- google-book-search-mern/src/components/Search/index.js
+++ google-book-search-mern/src/components/Search/index.js
@@ -3,7 +3,18 @@ import React from "react";
 function Search() {
     return (
         <div className="container">
-            <h1>Saved Books</h1>
+            <div className="card">
+                <div className="card-body">
+                    <h1>Book Search</h1>
+                    <h6>Books</h6>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Enter Book Info" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
