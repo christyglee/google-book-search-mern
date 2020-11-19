74ba5b161a14955e62084b48045463c1bcd0a532
Kelly Kim <kellykim408@gmail.com>
2020-11-19 13:37:37 -0800

    add components

---
+++ google-book-search-mern/src/components/PageTitleResults/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function PageTitleResults({ children }) {
+    return (
+        <p>
+            {children}
+        </p>
+    )
+}
+
+export default PageTitleResults;
