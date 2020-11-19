a4404338486dd01bd319bf20fec21f10ab34dca4
unknown <christy.g.lee@gmail.com>
2020-11-19 13:33:45 -0800

    description p-tag

---
+++ google-book-search-mern/src/components/Description/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Description({children}) {
+    return (
+        <p className="description">
+            {children}
+        </p>
+    )
+}
+
+export default Description;
