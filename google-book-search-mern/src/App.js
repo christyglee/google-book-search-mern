f6a7117bf56a706aa2171a31ee52fe21dd2022a4
Kelly Stone <kellystone916@gmail.com>
2020-11-19 16:16:32 -0800

    add saved page components

--- google-book-search-mern/src/App.js
+++ google-book-search-mern/src/App.js
@@ -3,15 +3,15 @@ import React from "react";
 import Header from "./components/Header";
 // import Search from "./components/Search";
 
-// import SavedPage from "./pages/SavedPage";
-import SearchPage from "./pages/SearchPage";
+import SavedPage from "./pages/SavedPage";
+// import SearchPage from "./pages/SearchPage";
 
 function App() {
     return (
         <div>
             <Header></Header>
-            <SearchPage></SearchPage>
-            {/* <SavedPage></SavedPage> */}
+            {/* <SearchPage></SearchPage> */}
+            <SavedPage></SavedPage>
 
 
 
