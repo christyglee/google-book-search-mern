3a70d74397bebc828ec49021e766b5d65085a2b2
unknown <christy.g.lee@gmail.com>
2020-11-20 14:56:46 -0800

    added scripts to package.json

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
 
 // Start the API server
